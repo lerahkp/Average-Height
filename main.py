c538f521c11fcd99390b936f744ada343909abea
lerahkp <52873528+lerahkp@users.noreply.github.com>
2021-03-18 09:50:07 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# 🚨 Don't change the code below 👇
+
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
 
 
-#Write your code below this row 👇
 sum_student_heights=0
 for heights in student_heights:
     sum_student_heights=heights+sum_student_heights
